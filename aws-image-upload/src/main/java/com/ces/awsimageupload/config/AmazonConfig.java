aae27f4b0416ae812695f2a75ebaf857c974448f
Cesar Lopez <cesarlerdeut@gmail.com>
2022-11-02 22:31:25 +0100

    Initial commit

---
+++ aws-image-upload/src/main/java/com/ces/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,27 @@
+
+package com.ces.awsimageupload.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AmazonConfig {
+
+  @Bean
+  public AmazonS3 s3() {
+    var awsCredentials = new BasicAWSCredentials(
+      "AKIASDM25CGVSY3LZMVS",
+      "Yg73D7RHr3aYxJCVjIykMXeRjkY7dFh8WeNceDvS"
+      );
+
+    return AmazonS3ClientBuilder
+      .standard()
+      .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+      .build();
+  }
+}
