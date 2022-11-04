e217eae77f1d4a48d968bc4dad7b96ff810f4e8b
Cesar Lopez <cesarlerdeut@gmail.com>
2022-11-05 00:09:34 +0100

    Create React app

--- aws-image-upload/src/main/java/com/ces/awsimageupload/profile/UserProfileService.java
+++ aws-image-upload/src/main/java/com/ces/awsimageupload/profile/UserProfileService.java
@@ -18,6 +18,12 @@ public class UserProfileService {
 
   public void uploadUserProfileIMage(UUID id, MultipartFile file) {
     throw new UnsupportedOperationException();
+    // 1. Check if image is not empty
+    // 2. If file is an image
+    // 3. The user exist in our DB
+    // 4. grab some metadata from file if any
+    // 5. Store image in S3
+    // 6. Update DB with S3, image and Link
   }
 
 }
