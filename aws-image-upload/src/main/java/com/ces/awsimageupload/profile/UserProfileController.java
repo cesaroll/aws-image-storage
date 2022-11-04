5d2623e00d9c2b08fd228b6aef6f75ad8929d342
Cesar Lopez <cesarlerdeut@gmail.com>
2022-11-04 23:47:15 +0100

    Upload image API

--- aws-image-upload/src/main/java/com/ces/awsimageupload/profile/UserProfileController.java
+++ aws-image-upload/src/main/java/com/ces/awsimageupload/profile/UserProfileController.java
@@ -1,10 +1,16 @@
 package com.ces.awsimageupload.profile;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AllArgsConstructor;
 
@@ -18,5 +24,19 @@ public class UserProfileController {
   @GetMapping
   public List<UserProfile> getUserProfiles() {
     return userProfileService.getUserProfiles();
+  }
+
+  @PostMapping(
+    path = "{id}/image/upload",
+    consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public void uploadUserProfileImage(
+    @PathVariable("id")
+    UUID id,
+    @RequestParam("file")
+    MultipartFile file
+  ) {
+    userProfileService.uploadUserProfileIMage(id, file);
   }
 }
--- aws-image-upload/src/main/java/com/ces/awsimageupload/profile/UserProfileService.java
+++ aws-image-upload/src/main/java/com/ces/awsimageupload/profile/UserProfileService.java
@@ -1,9 +1,9 @@
 package com.ces.awsimageupload.profile;
 
 import java.util.List;
-
+import java.util.UUID;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -14,6 +14,10 @@ public class UserProfileService {
 
   List<UserProfile> getUserProfiles() {
     return userProfileRepository.getUserProfiles();
+  }
+
+  public void uploadUserProfileIMage(UUID id, MultipartFile file) {
+    throw new UnsupportedOperationException();
   }
 
 }
