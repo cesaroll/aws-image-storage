0f189a4cbcdb3e9a72f72c2cc54c63f2b83fe8b4
Cesar Lopez <cesarlerdeut@gmail.com>
2022-11-06 01:13:08 +0100

    Better fetch user profiles

--- aws-image-upload/src/main/frontend/src/App.tsx
+++ aws-image-upload/src/main/frontend/src/App.tsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import UserProfiles from "./UserProfiles/components/UserProfiles";
-import { getUserProfiles } from "./UserProfiles/services/UserProfileService";
+import { fetchUserProfiles } from "./UserProfiles/services/UserProfileService";
 import { UserProfileInterface } from "./UserProfiles/types/UserProfileInterface";
 
 function App() {
   const [userProfiles, setUserProfiles] = useState<UserProfileInterface[]>([]);
 
-  const fetchUserProfiles = async () => {
-    const items = await getUserProfiles();
-    setUserProfiles(items);
-  };
-
   useEffect(() => {
-    fetchUserProfiles();
+    fetchUserProfiles(setUserProfiles);
   }, []);
 
   return (
--- aws-image-upload/src/main/frontend/src/UserProfiles/services/UserProfileService.ts
+++ aws-image-upload/src/main/frontend/src/UserProfiles/services/UserProfileService.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { Dispatch, SetStateAction } from "react";
 import { UserProfileInterface } from "../types/UserProfileInterface";
 
-export const getUserProfiles = async (): Promise<UserProfileInterface[]> => {
+const getUserProfiles = async (): Promise<UserProfileInterface[]> => {
   const { data, status } = await axios.get<UserProfileInterface[]>(
     "http://localhost:8080/api/v1/user-profile",
     {
@@ -15,4 +16,11 @@ export const getUserProfiles = async (): Promise<UserProfileInterface[]> => {
   // console.log(JSON.stringify(data, null, 4));
 
   return data;
+};
+
+export const fetchUserProfiles = async (
+  setUserProfiles: Dispatch<SetStateAction<UserProfileInterface[]>>
+) => {
+  const userProfiles = await getUserProfiles();
+  setUserProfiles(userProfiles);
 };
