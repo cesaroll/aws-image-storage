153e84322f3321ad792a59a895bb4a34b8242d57
Cesar Lopez <cesarlerdeut@gmail.com>
2022-11-08 19:39:18 +0100

    implement download Images in frontend

--- aws-image-upload/src/main/frontend/src/UserProfiles/components/UserProfile.tsx
+++ aws-image-upload/src/main/frontend/src/UserProfiles/components/UserProfile.tsx
@@ -10,6 +10,13 @@ const UserProfile = ({ userProfile }: UserProfileParms) => {
   return (
     <>
       <br />
+      {userProfile.id && (
+        <img
+          src={`http://localhost:8080/api/v1/user-profile/${userProfile.id}/image/download`}
+          alt=""
+        />
+      )}
+      <br />
       <h2>{userProfile.name}</h2>
       <p>{userProfile.id}</p>
       <DropZone {...userProfile} />
